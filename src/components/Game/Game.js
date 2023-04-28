fe7615df90e76e8208965e3d82fc97cb82a670fa
naeem161 <naeem.tahir2810@gmail.com>
2023-04-28 17:27:39 +0500

    changing color of words in grid

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,7 +7,7 @@ import UserInputs from '../UserInputs/UserInputs';
 
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+export const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from '../../game-helpers'
+import { answer } from '../Game'
 
 function Guess({ value }) {
 
+  const cl = checkGuess(value, answer)
 
   return (
     <>
       <p className="guess">
         {range(5).map((num) => (
-          <span key={num} className="cell">
+          <span key={num} className={`cell ${cl ? cl[num].status : undefined} `}>
             {value ? value[num] : undefined}
           </span>
         ))}
