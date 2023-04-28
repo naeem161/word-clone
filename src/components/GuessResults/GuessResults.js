639eed41d2dbdecdb8b65dc1c68c05983d5653d8
naeem161 <naeem.tahir2810@gmail.com>
2023-04-28 17:09:59 +0500

    showing user guess in grid

--- src/components/GuessResults/GuessResults.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function GuessResults() {
-  return <div />;
-}
-
-export default GuessResults;
--- src/components/UserInputs/UserInputs.js
+++ src/components/UserInputs/UserInputs.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Guess from '../Guess'
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 function UserInputs({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p
-          className="guess"
-          key={index}
-        >{guess}</p>
-      ))}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+
+        <Guess key={num} value={guesses[num]} />
+      )
+      )}
     </div>
   )
 }
